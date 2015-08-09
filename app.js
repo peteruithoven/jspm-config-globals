0913c6ba734510269253fa67040526a8e7ca37e2
peteruithoven <peter@peteruithoven.nl>
2015-08-09 22:29:54 +0200

    Just import user in app

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-import SOME_GLOBAL from './global-creator.js';
-console.log('SOME_GLOBAL: ', SOME_GLOBAL);
-console.log('SOME_GLOBAL.foo: ', SOME_GLOBAL.foo);
-
-// the following fails because SOME_GLOBAL can't be found
-import GlobalUser from './global-user.js';
-console.log('GlobalUser: ', GlobalUser);
-console.log('SOME_GLOBAL.globalUser: ', SOME_GLOBAL.globalUser);
-var globalUser = new SOME_GLOBAL.globalUser();
-console.log('globalUser.foo: ', globalUser.foo);
+import GlobalUser from './global-user/index.js';
+// import './global-user.js';
+// console.log('GlobalUser: ', GlobalUser);
+//console.log('SOME_GLOBAL: ', SOME_GLOBAL);
+// console.log('SOME_GLOBAL.globalUser: ', SOME_GLOBAL.globalUser);
