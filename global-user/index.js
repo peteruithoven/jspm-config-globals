2bf7b12b3daaafdc461712548d33a5457cf86d41
peteruithoven <peter@peteruithoven.nl>
2015-08-09 22:26:41 +0200

    Moved user to folder

---
+++ global-user/index.js
@@ -0,0 +1,10 @@
+console.log('user: SOME_GLOBAL: ', SOME_GLOBAL);
+// console.log('user: SOME_GLOBAL.foo: ', SOME_GLOBAL.foo);
+//
+// SOME_GLOBAL.globalUser = function () {
+//   console.log('user: SOME_GLOBAL.globalUser created');
+// };
+//
+// SOME_GLOBAL.globalUser.prototype.foo = function () {
+//   return 'globalUser:bar';
+// };
